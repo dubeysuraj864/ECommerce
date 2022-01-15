8834082f4828148fbee35dab4c43f2497af2fdbf
Suraj Dubey <dubeysuraj864@gmail.com>
2022-01-15 12:03:57 +0530

    first

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="We love, we love Yuvi!!!"/>
+   <div class="header">
+     <div class="container">
+       <div class="navbar">
+         <div class="logo">
+           <img src="./assets/logo-shopping.png" alt="logo" width="70">
+         </div>
+
+         <nav>
+           <ul>
+             <li><a href="#">Home</a></li>
+             <li><a href="#">Products</a></li>
+             <li><a href="#">About</a></li>
+             <li><a href="#">Contact</a></li>
+             <li><a href="#">Account</a></li>
+           </ul>
+         </nav>
+       </div>
+     </div>
+   </div>
+
+  <HelloWorld msg="Ecommerce Shit"/>
 </template>
 
 <script>
@@ -15,6 +34,23 @@ export default {
 </script>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+/* body{
+} */
+.navbar{
+  display: flex;
+  align-items: center;
+   padding: 20px;
+}
+nav{
+  flex: 1;
+  text-align: right;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
